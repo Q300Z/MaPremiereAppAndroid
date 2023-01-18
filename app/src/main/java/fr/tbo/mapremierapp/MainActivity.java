771a10f1f96783236c50efeb2895bb3f7b26c0b8
Q300Z <Q300Zhomas@gmail.com>
2023-01-18 22:39:49 +0100

    Changement de text dans les éléments : zText et iButton

--- app/src/main/java/fr/tbo/mapremierapp/MainActivity.java
+++ app/src/main/java/fr/tbo/mapremierapp/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         mPlayButton.setEnabled(false);
 
-        mNameEditText.addTextChangedListener();
 
         mNameEditText.addTextChangedListener(new TextWatcher() {
             @Override
