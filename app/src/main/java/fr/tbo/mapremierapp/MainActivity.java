47a277b741e30996f1e0b41e840b90575bfdaaf7
Q300Z <Q300Zhomas@Gmail.com>
2023-01-18 22:19:47 +0100

    Mise en git

---
+++ app/src/main/java/fr/tbo/mapremierapp/MainActivity.java
@@ -0,0 +1,58 @@
+package fr.tbo.mapremierapp;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGreetingTextView = findViewById(R.id.tMain);
+        mNameEditText = findViewById(R.id.zText);
+        mPlayButton = findViewById(R.id.iButton);
+
+        mPlayButton.setEnabled(false);
+
+        mNameEditText.addTextChangedListener();
+
+        mNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This is where we'll check the user inputs
+                mPlayButton.setEnabled(!s.toString().isEmpty());
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The user just clicked
+            }
+        });
+
+
+    }
+
+
+}
